fa4b17d9aeaed9be25591d90a75e5e9ece3de88b
Christopher Lee <crlapples19@gmail.com>
2025-03-17 11:36:11 -0700

    A

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
