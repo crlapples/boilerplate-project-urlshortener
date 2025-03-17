2bdde729b9800e66dc48fa5758b8686df7365ba7
Christopher Lee <crlapples19@gmail.com>
2025-03-17 11:29:56 -0700

    A

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { nanoid } = require("nanoid");
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -21,4 +22,29 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+
+function isValid(string) {
+  try {
+    const url = new URL(string);
+    return url.protocol === "http:" || url.protocol === "https:";
+  } catch (_) {
+    return false;
+  }
+}
+
+const urls = {};
+
+app.post("/api/shorturl", function (req, res) {
+  if (!isValid(req.body.url)) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+  const id = nanoid(2);
+  urls[id] = req.body.url;
+  res.json({ original_url : req.body.url, short_url : `${req.protocol}://${req.get("host")}/api/shorturl/${id}` })
+});
+
+app.get("/api/shorturl/:id", function(req, res) {
+  res.redirect(urls[req.params.id]);
 });
